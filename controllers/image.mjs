4136ad312d01df23c8f805827367d3000648bd35
neven3 <61207254+neven3@users.noreply.github.com>
2020-07-04 01:15:40 +0200

    Try omitting dotenv entirely

--- controllers/image.mjs
+++ controllers/image.mjs
@@ -1,7 +1,7 @@
 import Clarifai from 'clarifai';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const app = new Clarifai.App({
     apiKey: '6e58104673d1490e8d67f9da0f283cd3'
--- server.mjs
+++ server.mjs
@@ -6,15 +6,15 @@ import handleRegister from './controllers/register.mjs';
 import handleSignin from './controllers/signin.mjs';
 import handleProfile from './controllers/profile.mjs';
 import {handleImage, handleImageUrl} from './controllers/image.mjs';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const postgres = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        // ssl: true,
     },
 });
 
